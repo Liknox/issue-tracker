80d592903a4d176c36bfd619d3115b4b1f94558a
Nazar Koval <n.kovaldev@gmail.com>
2023-04-05 23:32:56 +0300

    feat: labels filter
    In this commit I created state for labels
    
    Also I added onClick action to the label list which add or remove particular labelID to the state
    
    Also I adjusted issues api endpoint in order to output only required issues with the certain labels

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,8 +1,11 @@
 import { useQuery } from "react-query"
 import { IssueItem } from "./IssueItem"
 
-export default function IssuesList() {
-	const issuesQuery = useQuery(["issues"], () => fetch("/api/issues").then(res => res.json()))
+export default function IssuesList({ labels }) {
+	const issuesQuery = useQuery(["issues", { labels }], () => {
+		const labelsString = labels.map(e => `labels[]=${e}`).join("&")
+		return fetch(`/api/issues?${labelsString}`).then(res => res.json())
+	})
 	return (
 		<div>
 			<h2>Issues List</h2>
@@ -11,7 +14,17 @@ export default function IssuesList() {
 			) : (
 				<ul className="issues-list">
 					{issuesQuery.data.map(issue => (
-						<IssueItem key={issue.id} title={issue.title} number={issue.number} assignee={issue.assignee} commentCount={issue.comments.length} createdBy={issue.createdBy} createdDate={issue.createdDate} labels={issue.labels} status={issue.status} />
+						<IssueItem
+							key={issue.id}
+							title={issue.title}
+							number={issue.number}
+							assignee={issue.assignee}
+							commentCount={issue.comments.length}
+							createdBy={issue.createdBy}
+							createdDate={issue.createdDate}
+							labels={issue.labels}
+							status={issue.status}
+						/>
 					))}
 				</ul>
 			)}
--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,23 @@
-export default function LabelList() {
-	return <h3>Labels</h3>
+import { useLabelsData } from "../helpers/useLabelsData"
+
+export default function LabelList({ selected, toggle }) {
+	const labelsQuery = useLabelsData()
+	return (
+		<div className="labels">
+			<h3>Labels</h3>
+			{labelsQuery.isLoading ? (
+				<p>Loading</p>
+			) : (
+				<ul>
+					{labelsQuery.data.map(e => (
+						<li key={e.id}>
+							<button onClick={() => toggle(e.id)} className={`label ${selected.includes(e.id) ? "selected" : ""} ${e.color}`}>
+								{e.name}
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	)
 }
