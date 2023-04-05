2145e777ae54e219ef316abb3951c07dd48de69c
Nazar Koval <n.kovaldev@gmail.com>
2023-04-05 22:16:50 +0300

    feat: adjust appearing labels
    In this commit I created render labels with their own colors.

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -2,27 +2,38 @@ import { Link } from "react-router-dom"
 import { GoIssueOpened, GoIssueClosed, GoComment } from "react-icons/go"
 import { relativeDate } from "../helpers/relativeDate"
 import { useUserData } from "../helpers/useUserData"
+import { Label } from "./Label"
 
 export function IssueItem({ title, number, assignee, commentCount, createdBy, createdDate, labels, status }) {
 	const assigneeUser = useUserData(assignee)
 	const createdByUser = useUserData(createdBy)
 	return (
 		<li>
-			<div>{status === "done" || status === "cancelled" ? <GoIssueClosed style={{ color: "lime" }} /> : <GoIssueOpened style={{ color: "lime" }} />}</div>
+			<div>
+				{status === "done" || status === "cancelled" ? (
+					<GoIssueClosed style={{ color: "lime" }} />
+				) : (
+					<GoIssueOpened style={{ color: "lime" }} />
+				)}
+			</div>
 			<div className="issue-content">
 				<span>
 					<Link to={`/issue/${number}`}>{title}</Link>
 					{labels.map(label => (
-						<span key={label} className={`label red`}>
-							{label}
-						</span>
+						<Label key={label} label={label} />
 					))}
 				</span>
 				<small>
 					#{number} opened {relativeDate(createdDate)} by {createdByUser.isSuccess ? `by ${createdByUser.data.name}` : ""}
 				</small>
 			</div>
-			{assignee ? <img src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""} className="assigned-to" alt={`assigned to ${assigneeUser.isSuccess ? assigneeUser.data.name : ""}`} ></img> : null}
+			{assignee ? (
+				<img
+					src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""}
+					className="assigned-to"
+					alt={`assigned to ${assigneeUser.isSuccess ? assigneeUser.data.name : ""}`}
+				></img>
+			) : null}
 			<span className="comment-count">
 				{commentCount > 0 ? (
 					<>
--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query"
 
 export const useUserData = userId => {
+   if (userId === null) return null
 	const userData = useQuery(["users", userId], () => fetch(`/api/users/${userId}`).then(res => res.json()))
 
 	return userData
